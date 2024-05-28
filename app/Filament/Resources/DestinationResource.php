f685c6ce098eb636dea5d97be5b246d065abd13f
mproyyan <mproyyan@gmail.com>
2024-05-28 22:09:24 +0700

    feat: create model and filament resource for destination

---
+++ app/Filament/Resources/DestinationResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DestinationResource\Pages;
+use App\Models\Destination;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class DestinationResource extends Resource
+{
+    protected static ?string $model = Destination::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Nama')
+                            ->required(),
+
+                        Textarea::make('description')
+                            ->label('Deskripsi')
+                            ->autosize()
+                            ->required(),
+                    ]),
+
+                Section::make('Foto')
+                    ->schema([
+                        FileUpload::make('image')
+                            ->hiddenLabel()
+                            ->image()
+                            ->imageEditor()
+                            ->imageEditorAspectRatios([
+                                null,
+                                '16:9',
+                                '4:3',
+                                '1:1',
+                            ]),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('name'),
+
+                TextColumn::make('description')
+                    ->label('Deskripsi')
+                    ->limit(50),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDestinations::route('/'),
+            'create' => Pages\CreateDestination::route('/create'),
+            'edit' => Pages\EditDestination::route('/{record}/edit'),
+        ];
+    }
+}
