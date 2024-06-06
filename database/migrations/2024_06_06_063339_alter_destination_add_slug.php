75e8277df24c81abeb9dc6d21919aea9ee6a3c81
mproyyan <mproyyan@gmail.com>
2024-06-06 13:57:19 +0700

    fix: error typo when adding slug column

--- database/migrations/2024_06_06_063339_alter_destination_add_slug.php
+++ database/migrations/2024_06_06_063339_alter_destination_add_slug.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('destinations', function (Blueprint $table) {
+        Schema::table('destinations', function (Blueprint $table) {
             $table->string('slug')->after('name');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::create('destinations', function (Blueprint $table) {
+        Schema::table('destinations', function (Blueprint $table) {
             $table->dropColumn('slug');
         });
     }
