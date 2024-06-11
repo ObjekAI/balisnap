a59af57be9e298aba390691758fb6e6315d40af7
mproyyan <mproyyan@gmail.com>
2024-06-11 10:06:29 +0700

    feat: create food resource

---
+++ app/Filament/Resources/FoodResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FoodResource\Pages;
+use App\Models\Food;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class FoodResource extends Resource
+{
+    protected static ?string $model = Food::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-storefront';
+
+    protected static ?string $navigationLabel = 'Foods';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Nama')
+                            ->required(),
+
+                        Textarea::make('description')
+                            ->label('Deskripsi')
+                            ->autosize(),
+                    ]),
+
+                Section::make('Foto')
+                    ->schema([
+                        FileUpload::make('image')
+                            ->disk('gcs')
+                            ->directory('images')
+                            ->hiddenLabel()
+                            ->image()
+                            ->imageEditor()
+                            ->imageEditorAspectRatios([
+                                null,
+                                '16:9',
+                                '4:3',
+                                '1:1',
+                            ]),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nama'),
+
+                TextColumn::make('description')
+                    ->label('Deskripsi')
+                    ->limit(50),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFood::route('/'),
+            'create' => Pages\CreateFood::route('/create'),
+            'edit' => Pages\EditFood::route('/{record}/edit'),
+        ];
+    }
+}
